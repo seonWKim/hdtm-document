b709310c23e1875b47f58acf3689831365ab70d3
김선우 <seonwoo960000@toss.im>
2024-12-14 12:43:43 +0900

    Add toastmaster logo, svg, png

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'HDTM',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -68,9 +68,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'HDTM',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'HDTM Logo',
         src: 'img/logo.svg',
       },
       items: [
