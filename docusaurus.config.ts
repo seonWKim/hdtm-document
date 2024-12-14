d46e1449b0e744c82edc50d2ad8b3d65efa940ed
김선우 <seonwoo960000@toss.im>
2024-12-14 14:40:40 +0900

    nit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -97,10 +97,6 @@ const config: Config = {
           title: 'Community',
           items: [
             {
-              label: 'Toastmaster International',
-              href: 'https://www.toastmasters.org/',
-            },
-            {
               label: 'Kakao',
               href: 'https://open.kakao.com/o/ggM3CWFb',
             },
@@ -116,6 +112,10 @@ const config: Config = {
             {
               "label": "Meeting Location - Aank Hotel Hongdae (B1)",
               "href": "https://www.google.com/maps/place/%EC%95%84%EB%8A%91%ED%98%B8%ED%85%94+%ED%99%8D%EB%8C%80%EC%A0%90/data=!3m1!4b1!4m9!3m8!1s0x357c98dc98502d37:0x30914c6d11b603df!5m2!4m1!1i2!8m2!3d37.5555353!4d126.920378!16s%2Fg%2F11s911n39m?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"
+            },
+            {
+              label: 'Toastmaster International',
+              href: 'https://www.toastmasters.org/',
             },
             {
               label: 'GitHub',
