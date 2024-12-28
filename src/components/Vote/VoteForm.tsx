11c3644e13efca0540b47bf9e7b1fce8d408c282
김선우 <seonwoo960000@toss.im>
2024-12-28 15:45:19 +0900

    add vote

---
+++ src/components/Vote/VoteForm.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const VoteForm: React.FC = () => {
+    return (
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSf13Lk2ZJDfb0PI7s49CpVpw9Y_rWxHQIDX7FvhZxnZD2YKIg/viewform?embedded=true"
+            width="640"
+            height="1814"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
+        >
+            Loading…
+        </iframe>
+    );
+};
+
+export default VoteForm;
