5de6224cd141bae67b9c8ce826856eab014e257a
김선우 <seonwoo960000@toss.im>
2024-12-29 15:25:11 +0900

    Add pathway

--- docs/others/speech_contest.md
+++ docs/others/speech_contest.md
@@ -1,5 +1,5 @@
 ---
-sidebar_position: 3
+sidebar_position: 4
 description: Overview of the speech contest, including roles, requirements, and rules.
 ---
 
---
+++ src/components/Pathway/Pathway.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Pathway: React.FC = () => {
+  return (
+      <iframe
+          src="https://drive.google.com/file/d/12hF4mxJ5Fchaga8kYEwUQv_UmqK4Cao2/preview"
+          width="640"
+          height="480"
+          allow="autoplay"
+      >
+        Loading…
+      </iframe>
+  );
+};
+
+export default Pathway;
