4bcf3343224122f0bcc1e4e9a57a16f1f222916a
김선우 <seonwoo960000@toss.im>
2025-01-04 19:43:55 +0900

    Fix Pathway.tsx css

--- src/components/Pathway/Pathway.tsx
+++ src/components/Pathway/Pathway.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
 
 const Pathway: React.FC = () => {
-  return (
-      <iframe
-          src="https://drive.google.com/file/d/12hF4mxJ5Fchaga8kYEwUQv_UmqK4Cao2/preview"
-          width="640"
-          height="480"
-          allow="autoplay"
-      >
-        Loading…
-      </iframe>
-  );
+    return (
+        <div style={containerStyle}>
+            <iframe
+                src="https://drive.google.com/file/d/12hF4mxJ5Fchaga8kYEwUQv_UmqK4Cao2/preview"
+                style={iframeStyle}
+                width="640"
+                height="480"
+                allow="autoplay"
+            >
+                Loading…
+            </iframe>
+        </div>
+    );
+};
+
+const containerStyle: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    '@media only screen and (min-width: 900px)': {
+        flexDirection: 'row',
+    },
+};
+
+const iframeStyle: React.CSSProperties = {
+    width: '100%',
+    margin: '10px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', // Add elevation
+    '@media only screen and (min-width: 900px)': {
+        width: '50%',
+    },
 };
 
 export default Pathway;
