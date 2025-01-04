4c4a595b9d9ec6c4112091ce9f8a1460d04000eb
김선우 <seonwoo960000@toss.im>
2025-01-04 09:22:46 +0900

    Nit css

--- src/components/Agenda/AgendaGoogleDocs.tsx
+++ src/components/Agenda/AgendaGoogleDocs.tsx
@@ -2,14 +2,33 @@ import React from 'react';
 
 const AgendaGoogleDocs: React.FC = () => {
   return (
-    <iframe
-      src="https://docs.google.com/spreadsheets/d/1iFlxdbKlpt89oWt9oZSxHJP8-bclmeDlyd6iz0S_LKM/edit?usp=sharing&embedded=true"
-      width="1048"
-      height="1414"
-    >
-      Loading…
-    </iframe>
+      <div style={containerStyle}>
+        <iframe
+            src="https://docs.google.com/spreadsheets/d/1iFlxdbKlpt89oWt9oZSxHJP8-bclmeDlyd6iz0S_LKM/edit?usp=sharing&embedded=true"
+            style={iframeStyle}
+            width="1048"
+            height="1414"
+        >
+          Loading…
+        </iframe>
+      </div>
   );
+};
+
+const containerStyle: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  '@media only screen and (min-width: 900px)': {
+    flexDirection: 'row',
+  },
+};
+
+const iframeStyle: React.CSSProperties = {
+  width: '100%',
+  margin: '10px',
+  '@media only screen and (min-width: 900px)': {
+    width: '50%',
+  },
 };
 
 export default AgendaGoogleDocs;
