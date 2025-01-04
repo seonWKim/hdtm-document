b44be66cd6d1e8940072a1247fd57b76998caa4b
김선우 <seonwoo960000@toss.im>
2025-01-04 17:38:11 +0900

    Update 404 redirection logic

---
+++ src/theme/NotFound/index.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import { useHistory } from '@docusaurus/router';
+
+// Redirect to the introduction page whenever user visits invalid page
+export default function NotFoundWrapper(): JSX.Element {
+    const history = useHistory();
+
+    useEffect(() => {
+        history.push('/hdtm-document/docs/intro');
+    }, [history]);
+
+    return null;
+}
